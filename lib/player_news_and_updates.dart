419ccfcf6e0d5e3ad89aa529ae089295b8ecb3c4
Aldbas <44149366+Aldbas@users.noreply.github.com>
2019-06-24 11:48:34 -0700

    playerupdates
    different method to get player updates

--- lib/player_news_and_updates.dart
+++ lib/player_news_and_updates.dart
@@ -1,23 +1,37 @@
+import 'dart:convert';
+import 'dart:math';
+import 'package:html/parser.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:html/parser.dart' as parser;
+import 'package:html/dom.dart' as dom;
 import 'package:http/http.dart' as http;
 import 'package:webfeed/webfeed.dart';
 import 'package:xml/xml.dart' as xml;
+import 'package:xml2json/xml2json.dart';
+
 import 'xml test.dart';
 
 List<String> items = [];
 var client = http.Client();
+final Xml2Json xml2json = Xml2Json();
 
-hello() {
-  client
-      .get("https://developer.apple.com/news/releases/rss/releases.rss")
-      .then((response) {
-    return response.body;
-  }).then((bodyString) {
-    var channel = new RssFeed.parse(bodyString);
-    print(channel);
-    return channel;
-  });
+hello() async {
+  String url =
+      'http://premium.rotoworld.com/rss/feed.aspx?sport=nba&ftype=news&count=12&format=rss';
+  var response = await client.get(url);
+  dom.Document document = parser.parse(response.body);
+  var test = document.getElementsByTagName('description');
+  List<String> test2 = document
+      .getElementsByTagName('description')
+      .map((e) => (e.text))
+      .toList();
+
+//  xml2json.parse(response.body);
+//  var jsonData = xml2json.toGData();
+//  var data = json.decode(jsonData);
+  print(test2);
 }
 
 rssStream() {
@@ -37,29 +51,6 @@ class PlayerUpdates extends StatefulWidget {
   _PlayerUpdatesState createState() => _PlayerUpdatesState();
 }
 
-var bookshelfXml = '''<?xml version="1.0"?>
-    <bookshelf>
-      <book>
-        <title lang="english">Growing a Language</title>
-        <price>29.99</price>
-      </book>
-      <book>
-        <title lang="english">Learning XML</title>
-        <price>39.95</price>
-      </book>
-      <price>132.00</price>
-      <book>
-      <title lang="english"> HEY </title>
-      </book>
-      <book>
-        <title lang="english">Learning XML</title>
-        <price>39.95</price>
-      </book>
-    </bookshelf>''';
-var document = xml.parse(bookshelfXml);
-var titles = document.findAllElements('title');
-List<Widget> testText = titles.map((titles) => Text(titles.text)).toList();
-
 Future<String> fetchData(String url) async {
   final response = await http.get(url);
   if (response.statusCode == 200)
@@ -73,38 +64,55 @@ class _PlayerUpdatesState extends State<PlayerUpdates> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: FutureBuilder(
-      future: fetchData('https://www.rotowire.com/rss/news.htm?sport=nba'),
+        body: FutureBuilder(
+      future: fetchData(
+          'http://premium.rotoworld.com/rss/feed.aspx?sport=nba&ftype=news&count=12&format=rss'),
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
           return CircularProgressIndicator();
         }
-        var testd = RssFeed.parse(snapshot.data);
-        List<RssItem> titles = testd.items;
-        List<String> helol = titles.map((title) => title.title).toList();
-        List<String> bodytext = titles.map((body) => body.description).toList();
+        dom.Document document = parser.parse(snapshot.data);
+        var test = document.getElementsByTagName('description');
+        List<String> test1 = document
+            .getElementsByTagName('title')
+            .map((title) => title.text)
+            .skip(1)
+            .toList();
+        List<String> test2 = document
+            .getElementsByTagName('description')
+            .map((e) => e.text)
+            .skip(1)
+            .toList();
+
+//        var testXml2Json = jsonDecode(toJson);
+//        List<AtomItem> titles = testd.items;
+//        List<String> headLine = titles.map((title) => title.title).toList();
+//        String testBody = titles
+//            .where((body) => body is RssItem && !body.content.trim().isNotEmpty)
+//            .join('\n');
+//        List<String> bodyText = titles.map((body) => body.content).toList();
+//        List<String> date = titles.map((date) => date.published).toList();
 
         return ListView.builder(
-            itemCount: titles.length,
+            itemCount: test2.length,
             itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(helol[index]),
-                subtitle: Text(bodytext[index]),
-              );
+              return Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blue[900])),
+                  height: 150.0,
+//                  width: 50,
+                  child: ListTile(
+                    title: Text(
+                      test1[index],
+                      textScaleFactor: .90,
+                    ),
+                    subtitle: Text(
+                      test2[index],
+                      softWrap: true,
+                    ),
+                  ));
             });
       },
-    )
-////                  return ListView.builder(
-////                      itemCount:
-////                          1, // TODO:consider setState prevent length error
-////                      itemBuilder: (context, index) {
-////                        return ListTile(
-////                          title: Text(titless[index]),
-////                          subtitle: Text('${document.text}'),
-////                        );
-////                      });
-//                })
-            )); //TODO:Return
+    )); //TODO:Return
   }
 }
